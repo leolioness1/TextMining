6690004d02c49911af78c5c9591dac645fcc5ab8
carolinasimao <55488392+carolinasimao@users.noreply.github.com>
2020-03-05 16:40:07 +0000

    update function working

--- baseline.py
+++ baseline.py
@@ -1,4 +1,11 @@
-# Import Dictionary
+
+from nltk.corpus import stopwords
+from nltk.stem.wordnet import WordNetLemmatizer
+from nltk.tokenize import word_tokenize
+from nltk.corpus import wordnet
+from nltk.stem import SnowballStemmer
+from bs4 import BeautifulSoup
+import string
 from gensim.corpora.dictionary import Dictionary
 from collections import defaultdict
 import itertools
@@ -7,18 +14,11 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
 import os
+from tqdm import tqdm_notebook as tqdm
 import re
-from bs4 import BeautifulSoup
-from nltk.corpus import stopwords
-from nltk.stem.wordnet import WordNetLemmatizer
-from nltk.tokenize import word_tokenize
-from nltk.corpus import wordnet
-from nltk.stem import SnowballStemmer
-import string
 
-# importing and consolidating the data
+root = 'C:\\Users\\carol\\Documents\\Mestrado - Data Science\\Text Mining\\Project\\Corpora\\train'
 
-root = 'C:\\Users\\Leonor.furtado\\PycharmProjects\\TextMining\Corpora\\train'
 file_name_and_text={}
 for file in os.listdir(root):
     name = os.path.splitext(file)[0]
@@ -39,11 +39,12 @@ new = file_data["author"].str.split("/", n=1, expand=True)
 file_data["author"] = new[0]
 file_data["Title"] = new[1]
 
-# Language preprocessing
-
 stop = set(stopwords.words('portuguese'))
 exclude = set(string.punctuation)
 lemma = WordNetLemmatizer()
+stemmer = SnowballStemmer('portuguese')
+
+
 def clean(text_list, lemmatize, stemmer):
     """
     Function that a receives a list of strings and preprocesses it.
@@ -53,7 +54,7 @@ def clean(text_list, lemmatize, stemmer):
     :param stemmer: Tag to apply the stemmer if True.
     """
     updates = []
-    for j in range(len(text_list)):
+    for j in tqdm(range(len(text_list))):
         
         text = text_list[j]
         
@@ -64,7 +65,7 @@ def clean(text_list, lemmatize, stemmer):
         text = re.sub("[^a-zA-Z]", ' ', text)
         
         #REMOVE TAGS
-        text = BeautifulSoup(text,features="html.parser").get_text()
+        text = BeautifulSoup(text).get_text()
         
         if lemmatize:
             text = " ".join(lemma.lemmatize(word) for word in text.split())
@@ -77,288 +78,8 @@ def clean(text_list, lemmatize, stemmer):
     return updates
 
 def update_df(dataframe, list_updated):
-    dataframe.update(pd.DataFrame({"Text": list_updated}))
+    dataframe['text']=list_updated
     
 updates = clean(file_data["text"], lemmatize = True, stemmer = False)
 
 update_df(file_data, updates)
-
-
-#Random code
-# Create a Dictionary from the articles: dictionary
-dictionary = Dictionary()
-
-# Select the id for "computer": computer_id
-computer_id = file_name_and_text.token2id.get("computer")
-
-# Use computer_id with the dictionary to print the word
-print(dictionary.get(computer_id))
-
-# Create a MmCorpus: corpus
-corpus = [dictionary.doc2bow(article) for article in articles]
-
-# Print the first 10 word ids with their frequency counts from the fifth document
-print(corpus[4][:10])
-
-# Save the fifth document: doc
-doc = corpus[4]
-
-# Sort the doc for frequency: bow_doc
-bow_doc = sorted(doc, key=lambda w: w[1], reverse=True)
-
-# Print the top 5 words of the document alongside the count
-for word_id, word_count in bow_doc[:5]:
-    print(dictionary.get(word_id), word_count)
-
-# Create the defaultdict: total_word_count
-total_word_count = defaultdict(int)
-for word_id, word_count in itertools.chain.from_iterable(corpus):
-    total_word_count[word_id] += word_count
-
-# Create a sorted list from the defaultdict: sorted_word_count
-sorted_word_count = sorted(total_word_count.items(), key=lambda w: w[1], reverse=True)
-
-# Print the top 5 words across all documents alongside the count
-for word_id, word_count in sorted_word_count[:5]:
-    print(dictionary.get(word_id), word_count)
-
-# Import TfidfModel
-from gensim.models.tfidfmodel import TfidfModel
-# Create a new TfidfModel using the corpus: tfidf
-tfidf = TfidfModel(corpus)
-
-# Calculate the tfidf weights of doc: tfidf_weights
-tfidf_weights = tfidf[doc]
-
-# Print the first five weights
-print(tfidf_weights[:5])
-
-# Sort the weights from highest to lowest: sorted_tfidf_weights
-sorted_tfidf_weights = sorted(tfidf_weights, key=lambda w: w[1], reverse=True)
-
-# Print the top 5 weighted words
-for term_id, weight in sorted_tfidf_weights[:5]:
-    print(dictionary.get(term_id), weight)
-
-# Tokenize the article into sentences: sentences
-sentences = nltk.sent_tokenize(article)
-
-# Tokenize each sentence into words: token_sentences
-token_sentences = [nltk.word_tokenize(sent) for sent in sentences]
-
-# Tag each tokenized sentence into parts of speech: pos_sentences
-pos_sentences = [ nltk.pos_tag(sent) for sent in token_sentences]
-
-# Create the named entity chunks: chunked_sentences
-chunked_sentences = nltk.ne_chunk_sents(pos_sentences, binary=True)
-
-# Test for stems of the tree with 'NE' tags
-for sent in chunked_sentences:
-    for chunk in sent:
-        if hasattr(chunk, "label") and chunk.label() == "NE":
-            print(chunk)
-
-# Create the defaultdict: ner_categories
-ner_categories = defaultdict(int)
-# Create the nested for loop
-for sent in chunked_sentences:
-    for chunk in sent:
-        if hasattr(chunk, 'label'):
-            ner_categories[chunk.label()] += 1
-
-# Create a list from the dictionary keys for the chart labels: labels
-labels = list(ner_categories.keys())
-
-# Create a list of the values: values
-values = [ner_categories.get(l) for l in labels]
-
-# Create the pie chart
-plt.pie(values, labels=labels, autopct='%1.1f%%', startangle=140)
-
-# Display the chart
-plt.show()
-
-# Import spacy
-import spacy
-
-# Instantiate the English model: nlp
-nlp = spacy.load('en', tagger=False, parser=False, matcher=False)
-
-# Create a new document: doc
-doc = nlp(article)
-
-# Print all of the found entities and their labels
-for ent in doc.ents:
-    print(ent.label_, ent.text)
-
-from polygot.text import Text
-
-# Create a new text object using Polyglot's Text class: txt
-txt = Text(article)
-
-# Print each of the entities found
-for ent in txt.entities:
-    print(ent)
-
-# Print the type of ent
-print(type(ent))
-
-# Create the list of tuples: entities
-entities = []
-for ent in txt.entities:
-    entities.append((ent.tag, ' '.join(ent)))
-# Print entities
-print(entities)
-
-# Initialize the count variable: count
-count = 0
-
-# Iterate over all the entities
-for ent in txt.entities:
-    # Check whether the entity contains 'Márquez' or 'Gabo'
-    if 'Márquez' in ent or 'Gabo' in ent:
-        # Increment count
-        count += 1
-
-# Print count
-print(count)
-
-# Calculate the percentage of entities that refer to "Gabo": percentage
-percentage = count / len(txt.entities)
-print(percentage)
-
-# Import the necessary modules
-from sklearn.feature_extraction.text import CountVectorizer
-from sklearn.model_selection import train_test_split
-
-# Print the head of df
-print(df.head())
-
-# Create a series to store the labels: y
-y = df.label
-
-# Create training and test sets
-X_train, X_test, y_train, y_test = train_test_split(df['text'],y, test_size=0.33, random_state=53)
-
-# Initialize a CountVectorizer object: count_vectorizer
-count_vectorizer = CountVectorizer(stop_words="english")
-
-# Transform the training data using only the 'text' column values: count_train
-count_train = count_vectorizer.fit_transform(X_train)
-
-# Transform the test data using only the 'text' column values: count_test
-count_test = count_vectorizer.transform(X_test)
-
-# Print the first 10 features of the count_vectorizer
-print(count_vectorizer.get_feature_names()[:10])
-
-# Import TfidfVectorizer
-from sklearn.feature_extraction.text import TfidfVectorizer
-
-# Initialize a TfidfVectorizer object: tfidf_vectorizer
-tfidf_vectorizer = TfidfVectorizer(stop_words="english", max_df=0.7)
-
-# Transform the training data: tfidf_train
-tfidf_train = tfidf_vectorizer.fit_transform(X_train)
-
-# Transform the test data: tfidf_test
-tfidf_test = tfidf_vectorizer.transform(X_test)
-
-# Print the first 10 features
-print(tfidf_vectorizer.get_feature_names()[:10])
-
-# Print the first 5 vectors of the tfidf training data
-print(tfidf_train.A[:5])
-
-# Create the CountVectorizer DataFrame: count_df
-count_df = pd.DataFrame(count_train.A, columns=count_vectorizer.get_feature_names())
-
-# Create the TfidfVectorizer DataFrame: tfidf_df
-tfidf_df = pd.DataFrame(tfidf_train.A, columns=tfidf_vectorizer.get_feature_names())
-
-# Print the head of count_df
-print(count_df.head())
-
-# Print the head of tfidf_df
-print(tfidf_df.head())
-
-# Calculate the difference in columns: difference
-difference = set(count_df.columns) - set(tfidf_df.columns)
-print(difference)
-
-# Check whether the DataFrames are equal
-print(count_df.equals(tfidf_df))
-
-
-# Import the necessary modules
-from sklearn import metrics
-from sklearn.naive_bayes import MultinomialNB
-# Instantiate a Multinomial Naive Bayes classifier: nb_classifier
-nb_classifier = MultinomialNB()
-
-# Fit the classifier to the training data
-nb_classifier.fit(count_train,y_train)
-
-# Create the predicted tags: pred
-pred = nb_classifier.predict(count_test)
-
-# Calculate the accuracy score: score
-score = metrics.accuracy_score(y_test,pred)
-print(score)
-
-# Calculate the confusion matrix: cm
-cm = metrics.confusion_matrix(y_test,pred,labels=['FAKE','REAL'])
-print(cm)
-
-# Create a Multinomial Naive Bayes classifier: nb_classifier
-nb_classifier = MultinomialNB()
-
-# Fit the classifier to the training data
-nb_classifier.fit(tfidf_train, y_train)
-
-# Create the predicted tags: pred
-pred = nb_classifier.predict(tfidf_test)
-
-# Calculate the accuracy score: score
-score = metrics.accuracy_score(y_test, pred)
-print(score)
-
-# Calculate the confusion matrix: cm
-cm = metrics.confusion_matrix(y_test, pred,labels=['FAKE', 'REAL'] )
-print(cm)
-
-# Create the list of alphas: alphas
-alphas = np.arange(0,1,step=0.1)
-
-# Define train_and_predict()
-def train_and_predict(alpha):
-    # Instantiate the classifier: nb_classifier
-    nb_classifier = MultinomialNB(alpha=alpha)
-    # Fit to the training data
-    nb_classifier.fit(tfidf_train, y_train)
-    # Predict the labels: pred
-    pred = nb_classifier.predict(tfidf_test)
-    # Compute accuracy: score
-    score = metrics.accuracy_score(y_test,pred)
-    return score
-
-# Iterate over the alphas and print the corresponding score
-for alpha in alphas:
-    print('Alpha: ', alpha)
-    print('Score: ', train_and_predict(alpha))
-
-
-# Get the class labels: class_labels
-class_labels = nb_classifier.classes_
-
-# Extract the features: feature_names
-feature_names = tfidf_vectorizer.get_feature_names()
-
-# Zip the feature names together with the coefficient array and sort by weights: feat_with_weights
-feat_with_weights = sorted(zip(nb_classifier.coef_[0], feature_names))
-
-# Print the first class label and the top 20 feat_with_weights entries
-print(class_labels[0], feat_with_weights[:20])
-
-# Print the second class label and the bottom 20 feat_with_weights entries
-print(class_labels[1], feat_with_weights[-20:])
