2310ef6e6d833c0ac634096449b22455e05a8579
Leonor Furtado <36885311+leolioness1@users.noreply.github.com>
2020-04-05 20:31:41 +0100

    added lstm tuning file

--- lstm_tuning.py
+++ lstm_tuning.py
@@ -50,6 +50,7 @@ import torch
 import pickle
 import warnings
 import datetime
+from sklearn.model_selection import StratifiedKFold
 warnings.filterwarnings("ignore", category=DeprecationWarning)
 
 
@@ -110,6 +111,7 @@ new_file_data_1000.groupby(['author','title']).count().to_csv("number_of_1000_sa
 stop = set(stopwords.words('portuguese'))
 exclude = set(string.punctuation)
 lemma = WordNetLemmatizer()
+stemmer = SnowballStemmer('portuguese')
 
 
 def clean_data(dataframe):
@@ -146,6 +148,33 @@ update_df(file_data_test,cleaned_documents_test)
 
 file_data_test.insert(1,'y_true',['JoseSaramago','AlmadaNegreiros','LuisaMarquesSilva','EcaDeQueiros','CamiloCasteloBranco','JoseRodriguesSantos','JoseSaramago','AlmadaNegreiros','LuisaMarquesSilva','EcaDeQueiros','CamiloCasteloBranco','JoseRodriguesSantos'])
 
+
+###file_data nd clean documents only has non-alpha characters and html removed##
+#to be used for language modelling retains most text info##
+
+def stem_stop_words (dataframe):
+    processed_corpus = []
+    for i in range(len(dataframe)):
+        text = dataframe['text'][i]
+        # REMOVE STOP WORDS
+        text = text.split()
+        text = [stemmer.stem(word) for word in text if not word in stop]
+        text = " ".join(text)
+        processed_corpus.append(text)
+    return processed_corpus
+
+###stem_file_data and stem_documents also has stemming and stopwords removed##
+#to be used for NaiveBayes etc retains less text info##
+
+stem_documents = stem_stop_words(new_file_data)
+stem_documents_test = stem_stop_words(file_data_test)
+
+stem_file_data = new_file_data.copy(deep=True)
+stem_file_data_test = file_data_test.copy(deep=True)
+stem_file_data['text'] = stem_documents
+stem_file_data_test['text'] = stem_documents_test
+
+
 def lemma_stop_words (dataframe):
     processed_corpus = []
     for i in range(len(dataframe)):
@@ -169,23 +198,15 @@ lemma_file_data['text'] = lemma_documents
 lemma_file_data_test['text'] = lemma_documents_test
 
 # test = ['pg22801.txt','pg26103.txt','pg25641.txt','Furia Divina - Jose Rodrigues dos Santos.txt',
-#        'O Homem Duplicado - Jose Saramago.txt','UltimaHistoria.txt']
-#
-# X_train = lemma_file_data.loc[~lemma_file_data.title.isin(test)]
-# X_test = lemma_file_data.loc[lemma_file_data.title.isin(test)]
-#
-# y_train = np.array(lemma_file_data['author'].loc[~lemma_file_data.title.isin(test)])
-# y_test = np.array(lemma_file_data['author'].loc[lemma_file_data.title.isin(test)])
+#        'O Homem Duplicado - Jose Saramago.txt','UltimaHistoria.txt']pg22801.txt
+test=['pg22801.txt','pg27364.txt','pg25641.txt','O Setimo Selo - Jose Rodrigues dos Santos.txt','O Homem Duplicado - Jose Saramago.txt','ABelaHistoria.txt']
 
-############ word count #################
+new_file_data_subset = new_file_data.loc[~new_file_data.title.isin(test)]
+new_file_data_test = new_file_data.loc[new_file_data.title.isin(test)]
 
-word_count = file_data['text'].apply(lambda x: len(str(x).split(" ")))
-file_data['word_count']=word_count
-file_data[['text','word_count']].head()
+y_train = np.array(new_file_data['author'].loc[~new_file_data.title.isin(test)])
+y_test = np.array(new_file_data['author'].loc[new_file_data.title.isin(test)])
 
-all_words = ' '.join(file_data['text']).split()
-freq = pd.Series(all_words).value_counts()
-freq[:25]
 
 
 ################## KNN ######################
@@ -216,26 +237,26 @@ def KNN(train_df,test_df):
 
 KNN(lemma_file_data,lemma_file_data_test)
 
-################ Naive Bayes #################
-def NB(train_df,test_df):
-    cv_NB = CountVectorizer(max_df=0.9)
-    X_NB = cv_NB.fit_transform(train_df['text'])
-    y = np.array(train_df["author"])
-    X_train, X_test, y_train, y_test = train_test_split(X_NB, y, test_size = 0.2, random_state = 42)
-
-    nb_classifier = MultinomialNB()
-    # Fit the classifier to the training data
-    nb_classifier.fit(X_train,y_train)
-    X_NB_new = cv_NB.fit_transform(test_df['text'])
-    # Create the predicted tags: pred
-    predNB = nb_classifier.predict(X_test)
-    print(classification_report(y_test,predNB))
-    predNB_new = nb_classifier.predict(X_NB_new)
-    file_data_test["pred_NB"] = predNB_new
-    print(classification_report(file_data_test["y_true"],predNB_new))
-    return predNB_new
-
-NB(lemma_file_data,lemma_file_data_test)
+# ################ Naive Bayes #################
+# def NB(train_df,test_df):
+#     cv_NB = CountVectorizer(max_df=0.9)
+#     X_NB = cv_NB.fit_transform(train_df['text'])
+#     y = np.array(train_df["author"])
+#     X_train, X_test, y_train, y_test = train_test_split(X_NB, y, test_size = 0.2, random_state = 42)
+#
+#     nb_classifier = MultinomialNB()
+#     # Fit the classifier to the training data
+#     nb_classifier.fit(X_train,y_train)
+#     X_NB_new = cv_NB.fit_transform(test_df['text'])
+#     # Create the predicted tags: pred
+#     predNB = nb_classifier.predict(X_test)
+#     print(classification_report(y_test,predNB))
+#     predNB_new = nb_classifier.predict(X_NB_new)
+#     file_data_test["pred_NB"] = predNB_new
+#     print(classification_report(file_data_test["y_true"],predNB_new))
+#     return predNB_new
+#
+# NB(lemma_file_data,lemma_file_data_test)
 
 
 #EMBEDDINGS
@@ -249,7 +270,7 @@ vocab_size = len(vocabulary)
 print(vocab_size)
 
 # The maximum number of words to be used. (most frequent) or could use whole vocab size
-def LSTM_model(train_df,test_df,MAX_LEN,MAX_NB_WORDS,epochs,batch_size):
+def LSTM_model(train_df,test_df,new_df,MAX_LEN,MAX_NB_WORDS,epochs,batch_size):
 
     tokenizer = Tokenizer(num_words=MAX_NB_WORDS, lower=True)
     tokenizer.fit_on_texts(train_df.text.values)
@@ -261,16 +282,33 @@ def LSTM_model(train_df,test_df,MAX_LEN,MAX_NB_WORDS,epochs,batch_size):
     X = pad_sequences(X, maxlen=MAX_LEN)
     print('Shape of data tensor:', X.shape)
     Y = pd.get_dummies(train_df['author'])
-    X_train, X_test, y_train, y_test = train_test_split(X,Y, test_size=0.2, random_state=42, stratify=Y)
+    X_train, X_dev, y_train, y_dev = train_test_split(X, Y, test_size=0.1, random_state=7, stratify=Y)
     print(X_train.shape, y_train.shape)
-    print(X_test.shape, y_test.shape)
+    print(X_dev.shape, y_dev.shape)
+    # kfold_splits=10
+    # # Instantiate the cross validator
+    # skf = StratifiedKFold(n_splits=kfold_splits, shuffle=True,random_state=7)
+    # # Loop through the indices the split() method returns
+    # for index, (train_indices, val_indices) in enumerate(skf.split(X, Y.values.argmax(1))):
+    #     print
+    #     "Training on fold " + str(index + 1) + "/10..."
+    #     # Generate batches from indices
+    #     X_train, X_dev = X[train_indices], X[val_indices]
+    #     y_train, y_dev = Y[train_indices], Y[val_indices]
+        # Clear model, and create it
     model = Sequential()
     model.add(Embedding(input_dim=MAX_NB_WORDS, output_dim=100, input_length=X.shape[1]))
     model.add(LSTM(100))
     model.add(Dense(6, activation='softmax'))
     model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
 
-    history = model.fit(X_train, y_train, epochs=epochs, batch_size=batch_size, callbacks=[EarlyStopping(monitor='loss', patience=3, min_delta=0.01)])
+    # Debug message I guess
+    # print "Training new iteration on " + str(xtrain.shape[0]) + " training samples, " + str(xval.shape[0]) + " validation samples, this may be a while..."
+
+    history = model.fit(X_train, y_train, epochs=epochs, batch_size=batch_size,validation_data=(X_dev,y_dev),callbacks=[EarlyStopping(monitor='loss', patience=3, min_delta=0.01)])
+    # accuracy_history = history.history['acc']
+    # val_accuracy_history = history.history['val_acc']
+    # print( "Last training accuracy: " + str(accuracy_history[-1]) + ", last validation accuracy: " + str(val_accuracy_history[-1]))
 
     # save the model to disk
     filename = 'lstm_model_{}_{}.pkl'.format(MAX_LEN,datetime.datetime.today().strftime("%d_%m_%Y_%H_%M_%S"))
@@ -279,6 +317,9 @@ def LSTM_model(train_df,test_df,MAX_LEN,MAX_NB_WORDS,epochs,batch_size):
     # loaded_model = pickle.load(open(filename, 'rb'))
     # evaluate the model
     train_acc = model.evaluate(X_train, y_train, verbose=0)
+    # Change text for numerical ids and pad
+    X_test = tokenizer.texts_to_sequences(test_df.text)
+    X_test = pad_sequences(X_test, maxlen=MAX_LEN)
     test_acc = model.evaluate(X_test, y_test, verbose=0)
     print('---------------------------------------{} word samples------------------------------'.format(MAX_LEN))
     print('Train set\n  Loss: {:0.3f}\n  Accuracy: {:0.3f}'.format(train_acc[0],train_acc[1]))
@@ -292,7 +333,7 @@ def LSTM_model(train_df,test_df,MAX_LEN,MAX_NB_WORDS,epochs,batch_size):
     plt.show()
 
     # Change text for numerical ids and pad
-    X_new = tokenizer.texts_to_sequences(test_df.text)
+    X_new = tokenizer.texts_to_sequences(new_df.text)
     X_new = pad_sequences(X_new, maxlen=MAX_LEN)
 
     # Use the model to predict on new data
@@ -306,4 +347,4 @@ def LSTM_model(train_df,test_df,MAX_LEN,MAX_NB_WORDS,epochs,batch_size):
     print(classification_report(file_data_test['y_true'], predicted, target_names=Y.columns))
     return test_df.head()
 
-LSTM_model(new_file_data,file_data_test,1000,70000,5,32)
+LSTM_model(new_file_data_subset, new_file_data_test, file_data_test,1000,90000,5,32)
